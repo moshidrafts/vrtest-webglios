e00037828b553ea436826fe1a3e3e35ccda09313
Mudit jain <91784034+dontbethatMJ@users.noreply.github.com>
2025-10-28 19:30:30 +0530

    no av

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "The MANA House-360gyro-1.0.0";
 const contentToCache = [
-    "Build/av.loader.js",
-    "Build/av.framework.js.unityweb",
-    "Build/av.data.unityweb",
-    "Build/av.wasm.unityweb",
+    "Build/NOav.loader.js",
+    "Build/NOav.framework.js.unityweb",
+    "Build/NOav.data.unityweb",
+    "Build/NOav.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
